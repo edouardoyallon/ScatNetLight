98c6b9740df55d555b0e452ada22f87bd6840dc8
Matthew Hirn <matthew.hirn@ens.fr>
2016-07-18 08:28:43 -0400

    Edits to tutorial to help clarify things

--- scatnet_light/tutorial/scat_1d_tutorials.m
+++ scatnet_light/tutorial/scat_1d_tutorials.m
@@ -10,14 +10,14 @@
 % === Filters ===
 
 % Length of the signal
-N = 2^5;
+N = 2^7;
 
 % Filter options
 filt_opt.filter_type = 'morlet';
-filt_opt.translation.J = 5;
-filt_opt.n_wavelet_per_octave = 1;
+filt_opt.translation.J = log2(N);
+filt_opt.n_wavelet_per_octave = 2;
 filt_opt.xi_psi = 3*pi/4;
-filt_opt.sigma_phi = 0.8;
+filt_opt.sigma_phi = 0.34; % 0.30 (Q=1); 0.34 (Q=2); 
 filt_opt.sigma_psi = 0.8;
 
 % Create filters
