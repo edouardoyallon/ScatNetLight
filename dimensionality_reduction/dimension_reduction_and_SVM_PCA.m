aa7316a67dd17c89680c2b31899a4986f0ed69a3
CarmineCella <carmine.emanuele.cella@gmail.com>
2016-10-12 16:46:04 +0200

    added tictoc - ready for first real test

--- dimensionality_reduction/dimension_reduction_and_SVM_PCA.m
+++ dimensionality_reduction/dimension_reduction_and_SVM_PCA.m
@@ -3,29 +3,30 @@ C=option.Classification.C;
 SIGMA_v=option.Classification.SIGMA_v;
 
    
-    tic;
-    % Split in training and testing sets
-    q=split_function();
-    labels_train=q{1}(1:30);
-    labels_test=q{2};
-    
-    
-        outputTrainData=S_train;
-        outputTestData=S_test;
-  
-    % The bandwith of the kernel is given after renormalization
-    SIGMA=mean(sqrt(sum(outputTrainData.^2,1)))*SIGMA_v;
-    
-    % We build the kernel
-    kernel_test = kernelmatrix('rbf',outputTrainData,outputTestData,SIGMA);
-    kernel_train = kernelmatrix('rbf',outputTrainData,[],SIGMA);
-    [kernel_train,kernel_test]=prepare_kernel_for_svm(kernel_train,kernel_test);% Simple software for libsvm
-    
-    fprintf('\nNow going into the svm...\n')
-   
-    % Gives confusion_matrix
-    [confusion_matrix]=SVM_1vsALL_wrapper(labels_train,labels_test, kernel_train,kernel_test,C);score_function(confusion_matrix)
-    
+tic;
+% Split in training and testing sets
+q=split_function();
+%FIXME
+labels_train=q{1}(1:30);
+labels_test=q{2}(1:30);
+
+
+outputTrainData=S_train;
+outputTestData=S_test;
+
+% The bandwith of the kernel is given after renormalization
+SIGMA=mean(sqrt(sum(outputTrainData.^2,1)))*SIGMA_v;
+
+% We build the kernel
+kernel_test = kernelmatrix('rbf',outputTrainData,outputTestData,SIGMA);
+kernel_train = kernelmatrix('rbf',outputTrainData,[],SIGMA);
+[kernel_train,kernel_test]=prepare_kernel_for_svm(kernel_train,kernel_test);% Simple software for libsvm
+
+fprintf('\nNow going into the svm...\n')
+
+% Gives confusion_matrix
+[confusion_matrix]=SVM_1vsALL_wrapper(labels_train,labels_test, kernel_train,kernel_test,C);
+score_function(confusion_matrix)
 
 
 timeToClassify=toc;
